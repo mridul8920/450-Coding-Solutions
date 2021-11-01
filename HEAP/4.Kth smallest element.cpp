4958549e02004768568d1da477c74c637c400cbf
Mridul Kapoor <66908728+mridul8920@users.noreply.github.com>
2021-11-01 21:50:09 +0530

    Create 4.Kth smallest element.cpp

---
+++ HEAP/4.Kth smallest element.cpp
@@ -0,0 +1,17 @@
+class Solution{
+    public:
+    // arr : given array
+    // l : starting index of the array i.e 0
+    // r : ending index of the array i.e size-1
+    // k : find kth smallest element and return using this function
+    int kthSmallest(int arr[], int l, int r, int k) {
+        //code here
+         priority_queue<int> pq;
+        for (int i=r; i>=0; i--)
+        {
+            pq.push(arr[i]);
+            if (pq.size() > k)pq.pop();
+        }
+        return pq.top();
+    }
+};
